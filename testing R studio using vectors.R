32659184044d11be4b403661359a228f6c0ed38e
alfredozamora <jose.zamora@udem.edu>
2021-08-22 22:27:17 -0500

    new

---
+++ testing R studio using vectors.R
@@ -0,0 +1,10 @@
+git config --global user.email "alfredo.zamorag@outlook.com"
+git config --global user.name "Alfredo"
+
+#codes for creating vectors using c()function()
+  
+  z<-c(1, 2)          #Numeric
+  z<-c(TRUE,FALSE)    #logical
+  z<-c("x", "y", "z") #charachter
+  z<-1:10             #integer
+  z<-c(1+0i, 2_1i)    #complex
